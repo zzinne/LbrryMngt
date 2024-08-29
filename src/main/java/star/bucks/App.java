044cc1ad2c6a279daa3808b7467023a585921ad4
JJI <jiin2527@naver.com>
2024-08-29 18:37:14 +0900

    Initial commit

---
+++ src/main/java/star/bucks/App.java
@@ -0,0 +1,13 @@
+package star.bucks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
